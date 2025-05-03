de497764ac9e458b5c962447360fe00bfeea903d
safia457 <safia.zidoun@haw-hamburg.de>
2025-05-03 17:14:49 +0200

    Improved Upload Design

--- resources/views/livewire/file-upload.blade.php
+++ resources/views/livewire/file-upload.blade.php
@@ -1,14 +1,18 @@
-<div>
-    <form wire:submit.prevent="submit" enctype="multipart/form-data">
-        <input type="file" wire:model="files" multiple>
-        @error('file')
-            <span class="error">{{ $message }}</span>
-        @enderror
+<div class="flex items-start max-md:flex-col">
+    <div class="flex-1 self-stretch max-md:pt-6">
 
-        <flux:button class="w-full" variant="primary" wire:click="submit">Upload</flux:button>
-    </form>
+        <form class="flex flex-col gap-4 mt-5 w-full max-w-lg" wire:submit.prevent="submit" enctype="multipart/form-data">
+            <flux:input type="file" wire:model="files" label="Select files to upload" multiple />
+            @error('file')
+                <span class="error">{{ $message }}</span>
+            @enderror
 
-    @if (session()->has('message'))
-        <div>{{ session('message') }}</div>
-    @endif
+            <flux:button class="w-full" variant="primary" wire:click="submit">Upload</flux:button>
+        </form>
+
+        @if (session()->has('message'))
+            <div>{{ session('message') }}</div>
+        @endif
+
+    </div>
 </div>
