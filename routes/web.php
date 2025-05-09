868e06fcad3e9104c60e0e9e7807a138327bb12f
aco88 <patricia-lea.peuler@haw-hamburg.de>
2025-05-09 21:53:42 +0200

    Added Admin utton to sidebar

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,15 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
 });
 
 //Download Route
